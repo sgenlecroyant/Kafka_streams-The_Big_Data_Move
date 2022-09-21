09e0621d23922b6416bbd5bbddaeb9e778c8e5f8
sgenlecroyant <sgenlecroyant@gmail.com>
2022-09-21 23:29:08 +0200

    project set up

---
+++ src/main/java/com/sgen/kafkastreams/app/KafkaStreamsBigDataMoveApplication.java
@@ -0,0 +1,16 @@
+package com.sgen.kafkastreams.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.github.javafaker.Faker;
+
+@SpringBootApplication
+public class KafkaStreamsBigDataMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsBigDataMoveApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/sgen/kafkastreams/app/KafkaStreamsBigDataMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgen.kafkastreams.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsBigDataMoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
