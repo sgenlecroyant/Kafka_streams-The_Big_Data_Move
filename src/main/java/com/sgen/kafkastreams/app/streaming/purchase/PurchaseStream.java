594b595a204449f2cf779bc1d4f17d9c971ea789
sgenlecroyant <sgenlecroyant@gmail.com>
2022-09-28 13:17:04 +0200

    create the Purchase Stream Source processor

--- src/main/java/com/sgen/kafkastreams/app/streaming/purchase/PurchaseStream.java
+++ src/main/java/com/sgen/kafkastreams/app/streaming/purchase/PurchaseStream.java
@@ -1,14 +1,10 @@
 package com.sgen.kafkastreams.app.streaming.purchase;
 
-import java.util.Locale;
-
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.kstream.ValueMapper;
-import org.apache.kafka.streams.kstream.ValueMapperWithKey;
 
 import com.sgen.kafkastreams.app.streaming.config.GlobalKafkaStreamsConfig;
 import com.sgen.kafkastreams.app.streaming.runner.DefaultStreamsRunner;
@@ -30,17 +26,9 @@ public class PurchaseStream {
 		StreamsBuilder streamsBuilder = new StreamsBuilder();
 
 		// the source processor which is reading from a Kafka Topic: hello-world
-		KStream<String, String> sourceStream = streamsBuilder.stream("hello-world");
+		KStream<String, String> purchasesSourceStream = streamsBuilder.stream("purchase-transactions");
 		// sending the result back to a specific topic since Kafka Streams is from Kafka
 		// to Kafka
-
-		sourceStream.mapValues(new ValueMapper<String, String>() {
-
-			@Override
-			public String apply(String value) {
-				return value.toUpperCase();
-			}
-		}).to("hello-world-output");
 
 		// bulding the KafkaStreams instance to be able to start our Streaming App later
 		// on
