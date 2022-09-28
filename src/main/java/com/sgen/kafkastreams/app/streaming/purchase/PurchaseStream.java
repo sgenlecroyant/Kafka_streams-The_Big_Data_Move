62cd82fcc82a41c98fcb045b891690d968e67cfa
sgenlecroyant <sgenlecroyant@gmail.com>
2022-09-28 15:43:38 +0200

    streamed our purchases data: as simple strings and added registered the JavaTime Module for serialization and deserialization

--- src/main/java/com/sgen/kafkastreams/app/streaming/purchase/PurchaseStream.java
+++ src/main/java/com/sgen/kafkastreams/app/streaming/purchase/PurchaseStream.java
@@ -1,12 +1,19 @@
 package com.sgen.kafkastreams.app.streaming.purchase;
 
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.ValueMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sgen.kafkastreams.app.streaming.config.GlobalKafkaStreamsConfig;
+import com.sgen.kafkastreams.app.streaming.helloworld.DataProducer;
 import com.sgen.kafkastreams.app.streaming.runner.DefaultStreamsRunner;
 import com.sgen.kafkastreams.app.streaming.runner.StreamsRunner;
 
@@ -16,7 +23,7 @@ public class PurchaseStream {
 	public static ValueMapper<? super String, ? super String> valueMapper = (v) -> v;
 
 	public static void main(String[] args) {
-//		SpringApplication.run(KafkaStreamsBigDataMoveApplication.class, args);
+//		SpringApplication.run(PurchaseStream.class, args);
 
 		// creating the Global Configuration Settings instance by using the Singleton
 		// Design Pattern
@@ -25,18 +32,27 @@ public class PurchaseStream {
 
 		StreamsBuilder streamsBuilder = new StreamsBuilder();
 
+		// SERDES
+		Serde<String> keySerde = Serdes.String();
+		Serde<String> purchaseStringSerde = Serdes.String();
 		// the source processor which is reading from a Kafka Topic: hello-world
-		KStream<String, String> purchasesSourceStream = streamsBuilder.stream("purchases");
+		KStream<String, String> purchasesSourceStream = streamsBuilder.stream("purchases",
+				Consumed.with(keySerde, purchaseStringSerde));
 		// sending the result back to a specific topic since Kafka Streams is from Kafka
 		// to Kafka
-
+		purchasesSourceStream.to("purchase-transactions", Produced.with(keySerde, purchaseStringSerde));
 		// bulding the KafkaStreams instance to be able to start our Streaming App later
 		// on
 		KafkaStreams kafkaStreams = globalKafkaStreamsConfig.getKafkaStreamsInstance(streamsBuilder, streamsConfig);
 
 		StreamsRunner streamsRunner = new DefaultStreamsRunner(kafkaStreams);
 		streamsRunner.start();
-		
-		
+
+		DataProducer randomPurchaseProducer = new DataProducer();
+
+		while (true) {
+			randomPurchaseProducer.sendRandomPurchase();
+		}
+
 	}
 }
