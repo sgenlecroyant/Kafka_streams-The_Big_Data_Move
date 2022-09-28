d84d8b1d57d68eab28e41b2f8cb026abebb42fab
sgenlecroyant <sgenlecroyant@gmail.com>
2022-09-28 13:50:58 +0200

    made our producer async for faster operations

--- src/main/java/com/sgen/kafkastreams/app/streaming/helloworld/DataProducer.java
+++ src/main/java/com/sgen/kafkastreams/app/streaming/helloworld/DataProducer.java
@@ -3,16 +3,23 @@ package com.sgen.kafkastreams.app.streaming.helloworld;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.Future;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.javafaker.Faker;
 
 public class DataProducer {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private static final String BOOTSTRAP_SERVERS = "localhost:9092";
 	private static final Class<StringSerializer> DEFAULT_KEY_SERIALIZER = StringSerializer.class;
@@ -27,11 +34,18 @@ public class DataProducer {
 
 		String randomFirstName = this.getFakerApi().name().firstName();
 		Object greetingMessage = String.format("Hello %s !", randomFirstName);
-
 		ProducerRecord<String, Object> producerRecord = new ProducerRecord<String, Object>("hello-world",
 				greetingMessage);
 
-		this.kafkaProducer.send(producerRecord);
+		Future<RecordMetadata> asynchSendResult = this.kafkaProducer.send(producerRecord, new Callback() {
+
+			@Override
+			public void onCompletion(RecordMetadata metadata, Exception exception) {
+				if (exception != null) {
+					logger.error("Something went wrong while sending data !!!");
+				}
+			}
+		});
 
 	}
 
