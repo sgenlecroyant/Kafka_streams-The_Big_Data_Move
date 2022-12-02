6366692201422ac843ab807673cfed6a465d7aaa
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-02 22:19:51 +0200

    changed the streams flow

--- src/main/java/com/sgen/kafkastreams/app/streaming/helloworld/HelloWorldStreamsProcessing.java
+++ src/main/java/com/sgen/kafkastreams/app/streaming/helloworld/HelloWorldStreamsProcessing.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
